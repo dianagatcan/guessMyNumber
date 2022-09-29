17e0a846b3ea825d1d5b6079b4aff53f70c9d61f
dianagatcan <gatcan.diana@gmail.com>
2022-09-29 16:58:14 +0300

    add styles

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ const startButton = document.getElementById("start");
 const checkButton = document.getElementById("check");
 const restartButton = document.getElementById("restart");
 const input = document.getElementById("guess");
-const message = document.getElementById("message");
-const startMessage =
-  "You have 5 attempts to guess a random number between 0 and 20.";
-
+let message = document.getElementById("message");
 let randomNumber;
 let totalGuesses = 5;
 
 //generate random number when click on start game button
 startButton.addEventListener("click", () => {
-  message.innerHTML = startMessage;
   generateRandom();
+  message.innerHTML = "You have 5 attempts";
+  startButton.classList.add("disable");
+  input.disabled = false;
+  checkButton.disabled = false;
 });
 
 function generateRandom() {
@@ -21,14 +21,20 @@ function generateRandom() {
 }
 
 //check the guess
-checkButton.addEventListener("click", checkGuess);
+checkButton.addEventListener("click", () => {
+  checkGuess();
+});
 
 function checkGuess() {
   let guess = input.value;
+  console.log(randomNumber);
   totalGuesses -= 1;
   if (totalGuesses > 0) {
     if (guess == randomNumber) {
       message.innerHTML = `You won. Number was ${randomNumber}`;
+      checkButton.disabled = true;
+      input.disabled = true;
+      restartButton.classList.remove("disable");
     } else if (guess > randomNumber) {
       message.innerHTML = `The random number is less than ${guess}. You have ${totalGuesses} more attempts`;
     } else {
@@ -36,14 +42,19 @@ function checkGuess() {
     }
   } else {
     message.innerHTML = `You lost. Number was ${randomNumber}`;
+    checkButton.disabled = true;
+    input.disabled = true;
+    startButton.classList.remove("disable");
   }
+  input.value = "";
 }
 
 restartButton.addEventListener("click", () => {
   restartGame();
+  restartButton.classList.add("disable");
 });
 
 function restartGame() {
   totalGuesses = 5;
-  message.innerHTML = startMessage;
+  startButton.classList.remove("disable");
 }
